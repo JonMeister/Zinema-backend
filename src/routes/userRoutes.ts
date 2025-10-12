c9a6b9be7e4239cc663dfc4aa69c0009476eaa37
es-sai-fi <es.sai.fi24@gmail.com>
2025-10-11 22:12:18 -0500

    docs: added userRoutes and userController documentation

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -23,22 +23,44 @@ router.post("/register", (req, res) => userController.registerUser(req, res));
  * @body {string} email - The user's email.
  * @body {string} password - The user's password.
  * @access Public
-*/
+ */
 router.post("/login", (req, res) => userController.loginUser(req, res));
 
+/**
+ * @route GET /api/users/getUser
+ * @description Get the current logged-in user's data.
+ * @header {string} Authorization - Bearer token for authentication.
+ * @access Private
+ */
 router.get("/getUser", authenticateToken, (req, res) =>
   userController.getUser(req, res)
 );
 
+/**
+ * @route PUT /api/users/updateUser
+ * @description Update the current logged-in user's information.
+ * @header {string} Authorization - Bearer token for authentication.
+ * @body {string} [firstName] - Updated first name.
+ * @body {string} [lastName] - Updated last name.
+ * @body {number} [age] - Updated age.
+ * @body {string} [email] - Updated email.
+ * @access Private
+ */
 router.put("/updateUser", authenticateToken, (req, res) =>
   userController.updateUser(req, res)
 );
 
+/**
+ * @route DELETE /api/users/deleteUser
+ * @description Delete the current logged-in user's account.
+ * @header {string} Authorization - Bearer token for authentication.
+ * @access Private
+ */
 router.delete("/deleteUser", authenticateToken, (req, res) =>
   userController.deleteUser(req, res)
 );
 
 /**
- *  * Export the router instance to be mounted in the main routes file.
-*/
+ * Export the router instance to be mounted in the main routes file.
+ */
 export default router;
